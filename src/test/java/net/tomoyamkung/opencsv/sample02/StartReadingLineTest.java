86ef02d836086c59891dfb2f33916afc9264e455
tomoyamkung <tsuyoshi.sugiyama@gmail.com>
2014-01-28 16:04:30 +0900

    ファイルの読み込み開始行を指定する検証プログラムを作成したのでコミット。

---
+++ src/test/java/net/tomoyamkung/opencsv/sample02/StartReadingLineTest.java
@@ -0,0 +1,43 @@
+package net.tomoyamkung.opencsv.sample02;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import au.com.bytecode.opencsv.CSVParser;
+import au.com.bytecode.opencsv.CSVReader;
+
+public class StartReadingLineTest {
+	
+	private CSVReader sut;
+
+	@Before
+	public void setUp() throws Exception {
+		int lineNumber = 2; // ファイルの読み込み開始行
+		
+		sut = new CSVReader(new InputStreamReader(
+				StartReadingLineTest.class.getClassLoader().getResourceAsStream(
+						StartReadingLineTest.class.getSimpleName() + ".csv"),
+						"UTF-8"),
+						CSVParser.DEFAULT_SEPARATOR,
+		                CSVParser.DEFAULT_QUOTE_CHARACTER,
+		                lineNumber);
+	}
+
+	@Test
+	public void ファイルを3行目から読み込める() throws Exception {
+		List<String[]> actual = sut.readAll();
+		
+		assertThat("3行目から読み込むので、読み込んだファイルの行数は2である", actual.size(), is(2));
+		
+		assertThat("3行目の内容が正しい", Arrays.toString(actual.get(0)), is("[line3, ccc, 333]"));
+		assertThat("4行目の内容が正しい", Arrays.toString(actual.get(1)), is("[line4, ddd, 444]"));
+	}
+
+}
