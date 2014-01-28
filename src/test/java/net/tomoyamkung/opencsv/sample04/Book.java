a4babaecd579b83ee8527b661174d278b702f7ef
tomoyamkung <tsuyoshi.sugiyama@gmail.com>
2014-01-28 23:44:20 +0900

    HeaderColumnNameMappingStrategy を使った CSV → JavaBeans 変換を検証したのでコミット。

---
+++ src/test/java/net/tomoyamkung/opencsv/sample04/Book.java
@@ -0,0 +1,78 @@
+package net.tomoyamkung.opencsv.sample04;
+
+public class Book {
+	
+	/**
+	 * タイトル。
+	 */
+	private String title;
+	
+	/**
+	 * 出版社。
+	 */
+	private String publisher;
+
+	/**
+	 * ページ数。
+	 */
+	private int numberOfPages;
+
+	/**
+	 * デフォルトコンストラクタを定義しないと <code>InstantiationException</code> が発生し parse に失敗する。
+	 */
+	public Book() {}
+	
+	public Book(String title, String publisher, int numberOfPages) {
+		this.title = title;
+		this.publisher = publisher;
+		this.numberOfPages = numberOfPages;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	/**
+	 * 定義しないと <code>NullPointerException</code> が発生し parse に失敗する。
+	 * 
+	 * @param title
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	/**
+	 * 定義しないと <code>NullPointerException</code> が発生し parse に失敗する。
+	 * 
+	 * @param publisher
+	 */
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+	public int getNumberOfPages() {
+		return numberOfPages;
+	}
+
+	/**
+	 * 定義しないと <code>NullPointerException</code> が発生し parse に失敗する。
+	 * 
+	 * @param numberOfPages
+	 */
+	public void setNumberOfPages(int numberOfPages) {
+		this.numberOfPages = numberOfPages;
+	}
+
+	/**
+	 * フィールド名を取得する。
+	 * 
+	 * @return
+	 */
+	public static final String[] getFieldName() {
+		return new String[]{"title", "publisher", "numberOfPages"};
+	}
+}
---
+++ src/test/java/net/tomoyamkung/opencsv/sample04/HeaderColumnNameMappingStrategyTest.java
@@ -0,0 +1,66 @@
+package net.tomoyamkung.opencsv.sample04;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import au.com.bytecode.opencsv.bean.CsvToBean;
+import au.com.bytecode.opencsv.bean.HeaderColumnNameMappingStrategy;
+
+public class HeaderColumnNameMappingStrategyTest {
+	
+	private Reader reader;
+	private HeaderColumnNameMappingStrategy<Book> strategy;
+	
+	@Before
+	public void setUp() throws Exception {
+		strategy = new HeaderColumnNameMappingStrategy<Book>();
+		strategy.setType(Book.class);
+		
+		reader = new InputStreamReader(
+				HeaderColumnNameMappingStrategyTest.class.getClassLoader().getResourceAsStream(
+						HeaderColumnNameMappingStrategyTest.class.getSimpleName() + ".csv"), "UTF-8");
+	}
+
+	/**
+	 * HeaderColumnNameMappingStrategy を使って CSV を Bean に変換する。
+	 * 
+	 * HeaderColumnNameMappingStrategy は次の条件に一致するときに使用します。
+	 * 
+	 * <ul>
+	 * <li>読み込む CSV の1行目がヘッダになっていること</li>
+	 * <li>ヘッダに記述されている名称と、マッピングする Bean のフィールド名が一致していること</li>
+	 * </ul>
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void HeaderColumnNameMappingStrategyを使ってCSVをBeanに変換する() throws Exception {
+		CsvToBean<Book> sut = new CsvToBean<Book>();
+		List<Book> actual = sut.parse(strategy, reader);
+		
+		assertThat(actual.size(), is(3));
+		
+		Book book = actual.get(0);
+		assertThat(book.getTitle(), is("タイトル1"));
+		assertThat(book.getPublisher(), is("出版社A"));
+		assertThat(book.getNumberOfPages(), is(298));
+		
+		book = actual.get(1);
+		assertThat(book.getTitle(), is("タイトル2"));
+		assertThat(book.getPublisher(), is("出版社B"));
+		assertThat(book.getNumberOfPages(), is(2980));
+		
+		book = actual.get(2);
+		assertThat(book.getTitle(), is("タイトル3"));
+		assertThat(book.getPublisher(), is("出版社C"));
+		assertThat(book.getNumberOfPages(), is(29800));
+	}
+
+}
